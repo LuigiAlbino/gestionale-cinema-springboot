c71eccb3545ee635eb31ba0662319204f1616d66
Luigi Albino <luigialbino@hotmail.it>
2025-10-24 17:42:09 +0200

    24/10/25 - integrato metodo di ricerca sulla base dell'id utente

--- src/main/java/luigialbino/cinema/repository/PrenotazioneRepository.java
+++ src/main/java/luigialbino/cinema/repository/PrenotazioneRepository.java
@@ -1,9 +1,12 @@
 package luigialbino.cinema.repository;
 
 import luigialbino.cinema.model.entity.Prenotazione;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PrenotazioneRepository extends JpaRepository<Prenotazione, Long> {
+    Page<Prenotazione> findByUtenteId(Long utenteId, Pageable pageable);
 }

99c5cc10048ee4c153370335e8ba4d932b4618d0
Luigi Albino <luigialbino@hotmail.it>
2025-11-03 16:53:39 +0100

    03/11/25 - creato controller per inserimento prenotazione

--- src/main/java/luigialbino/cinema/controller/PrenotazioneController.java
+++ src/main/java/luigialbino/cinema/controller/PrenotazioneController.java
@@ -1,15 +1,14 @@
 package luigialbino.cinema.controller;
 
 import luigialbino.cinema.model.dto.PageResponseDTO;
+import luigialbino.cinema.model.dto.PrenotazioneRequestDTO;
 import luigialbino.cinema.model.dto.PrenotazioneResponseDTO;
 import luigialbino.cinema.service.impl.PrenotazioneService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/prenotazione")
@@ -21,5 +20,10 @@ public class PrenotazioneController {
     public ResponseEntity<PageResponseDTO<PrenotazioneResponseDTO>> leMiePrenotazioni(@PageableDefault(sort = "proiezione.dataOraInizio") Pageable pageable){
         PageResponseDTO<PrenotazioneResponseDTO> response = prenotazioneService.leMiePrenotazioni(pageable);
         return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/crea-prenotazione")
+    public ResponseEntity<PrenotazioneResponseDTO> creaPrenotazione(@RequestBody PrenotazioneRequestDTO dto){
+        return ResponseEntity.ok(prenotazioneService.creaPrenotazione(dto));
     }
 }

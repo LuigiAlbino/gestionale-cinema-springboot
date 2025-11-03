64c5710c2dbe2df33435d63cfa6b3caf5c2026b9
Luigi Albino <luigialbino@hotmail.it>
2025-11-03 16:52:32 +0100

    03/11/25 - eliminati metodi non in uso

--- src/main/java/luigialbino/cinema/mapper/PrenotazioneMapper.java
+++ src/main/java/luigialbino/cinema/mapper/PrenotazioneMapper.java
@@ -1,18 +1,7 @@
 package luigialbino.cinema.mapper;
 
-import luigialbino.cinema.model.dto.PrenotazioneDTO;
-import luigialbino.cinema.model.entity.Prenotazione;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring", uses = {UtenteMapper.class, ProiezioneMapper.class})
 public interface PrenotazioneMapper {
-
-    @Mapping(source = "utente", target = "utente")
-    @Mapping(source = "proiezione", target = "proiezione")
-    PrenotazioneDTO toDto(Prenotazione prenotazione);
-
-    @Mapping(source = "utente", target = "utente")
-    @Mapping(source = "proiezione", target = "proiezione")
-    Prenotazione toEntity(PrenotazioneDTO dto);
 }

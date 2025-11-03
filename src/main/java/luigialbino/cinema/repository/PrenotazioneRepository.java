2965f231e99f7fbe46eea9a4e5a36d2d55fc6462
Luigi Albino <luigialbino@hotmail.it>
2025-11-03 16:50:01 +0100

    03/11/25 - inserito metodo di conta delle prenotazioni presenti per una determinata proiezione

--- src/main/java/luigialbino/cinema/repository/PrenotazioneRepository.java
+++ src/main/java/luigialbino/cinema/repository/PrenotazioneRepository.java
@@ -4,9 +4,14 @@ import luigialbino.cinema.model.entity.Prenotazione;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PrenotazioneRepository extends JpaRepository<Prenotazione, Long> {
     Page<Prenotazione> findByUtenteId(Long utenteId, Pageable pageable);
+
+    @Query("SELECT SUM(p.postiPrenotati) FROM Prenotazione p WHERE p.proiezione.id = :proiezioneId")
+    Integer sumPostiPrenotatiByProiezioneId(@Param("proiezioneId") Long proiezioneId);
 }

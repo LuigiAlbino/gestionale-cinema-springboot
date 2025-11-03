0ebb3e34ba9f20688de00ab66d7f381bb0d1cf8d
Luigi Albino <luigialbino@hotmail.it>
2025-11-03 16:53:25 +0100

    03/11/25 - aggiunto metodo di creazione prenotazione nell interfaccia

--- src/main/java/luigialbino/cinema/service/api/IPrenotazioneService.java
+++ src/main/java/luigialbino/cinema/service/api/IPrenotazioneService.java
@@ -1,11 +1,13 @@
 package luigialbino.cinema.service.api;
 
 import luigialbino.cinema.model.dto.PageResponseDTO;
-import luigialbino.cinema.model.dto.PrenotazioneDTO;
+import luigialbino.cinema.model.dto.PrenotazioneRequestDTO;
 import luigialbino.cinema.model.dto.PrenotazioneResponseDTO;
 import org.springframework.data.domain.Pageable;
 
 public interface IPrenotazioneService {
 
     PageResponseDTO<PrenotazioneResponseDTO> leMiePrenotazioni(Pageable pageable);
+
+    PrenotazioneResponseDTO creaPrenotazione(PrenotazioneRequestDTO dto);
 }
